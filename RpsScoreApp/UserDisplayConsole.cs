a61dfab0784883c80ae4eaaa4cea7c170ba0acc9
adpead <adpead@gmail.com>
2026-07-01 14:09:34 +0200

    Finished Scoring

--- RpsScoreApp/UserDisplayConsole.cs
+++ RpsScoreApp/UserDisplayConsole.cs
@@ -9,17 +9,19 @@ namespace RpsScoreApp
         public void Display()
         {
 
-           int rounds = 5;
+            int rounds = 5;
+            int incorrectInput = 0;
+           // int allowedIncorrectInput = 5;
 
-           Console.WriteLine("Please Enter Player 1 Name:");
-           string player1 = Console.ReadLine();
+            Console.WriteLine("Please Enter Player 1 Name:");
+            string player1 = Console.ReadLine();
 
-           Console.Clear();
+            Console.Clear();
 
-           Console.WriteLine("Please Enter Player 2 Name:");
-           string player2 = Console.ReadLine();
+            Console.WriteLine("Please Enter Player 2 Name:");
+            string player2 = Console.ReadLine();
 
-           ScoreTracker tracker = new ScoreTracker(player1,player2);
+            ScoreTracker tracker = new ScoreTracker(player1, player2);
 
             int currentRound = 1;
             //for(int currentRound=1; currentRound <= rounds; currentRound++)
@@ -35,8 +37,8 @@ namespace RpsScoreApp
                 if (roundWinner == "1" || roundWinner == "2")
                 {
                     PlayerScore roundWinnerScore;
-                    
-                    if (roundWinner == "1")  
+
+                    if (roundWinner == "1")
                         roundWinnerScore = tracker.UpdateScore(Player.Player1);
                     else
                         roundWinnerScore = tracker.UpdateScore(Player.Player2);
@@ -44,15 +46,38 @@ namespace RpsScoreApp
                     Console.WriteLine($"Well Done {roundWinnerScore.GetPlayerName()}! Your score is: {roundWinnerScore.GetPlayerScore()} ");
 
                     currentRound++;
+
+                    incorrectInput = 0;
                 }
                 else
                 {
                     Console.WriteLine("Please Enter only (1) or (2).");
-                  
+
+                    incorrectInput++;
+
+                    if (incorrectInput >= 5)
+                    {
+                        Console.WriteLine("Go away!");
+                        return;
+                    }
                 }
-                
-              
+
+
             }
+
+         
+
+                PlayerScore overallWinner = tracker.GetWinner();
+
+                if (overallWinner != null)
+                {
+                    Console.WriteLine($"The overall winner is: {overallWinner.GetPlayerName()} ");
+                }
+                else
+                {
+                    Console.WriteLine("It is a tie");
+                }
+            
         }
     }
 }
